8a5eb24c6ffe7ae52a7f97c637712d5c52557b58
beki-jone <bekojone@gmail.com>
2024-01-31 19:24:13 +0300

    main

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../../Header/Header'
 import Footer from '../../Footer/Footer'
+import Banner from '../../Banner/Banner'
+import Rowlist from '../../Row/Rowlist'
 
 const Home = () => {
   return (
     <div>
      <Header/>
+     <Banner/>
+     <Rowlist/>
      <Footer/>
     </div>
   )
